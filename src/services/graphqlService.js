3ca8a260fe6de0b66a62aeab0107f7343092ce0f
nyunja <poptiff@gmail.com>
2025-06-08 19:31:28 +0300

    feat: Implement GraphQLService class to handle interaction with GraphQL API endpoint
    - `query` function: handles POST requests and authetication using jwt processing the JSON response.
    - `getUserInfo` fetches general user details like ID, login, email, and also includes transactions for XP and progress which contains grade.
    - `getUserXP` fetches all transactions of type "xp".
    - `getCompletedExercises(userId, eventId): get theh completed progress for a particular event
    - `getUserSkills` retrieves user skills data by filtering transactions where the type starts with "skills_", ordered by amount.
    
    This service centralizes the GraphQL data fetching logic, making it easier to manage and reuse throughtout the application

---
+++ src/services/graphqlService.js
@@ -0,0 +1,102 @@
+export class GraphQLService {
+    constructor() {
+        this.endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql"
+        this.token = localStorage.getItem('jwt') || null;
+    }
+
+    async query(queryStr, variables = {}) {
+        try {
+            const response = await fetch(this.endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.token}`
+                },
+                body: JSON.stringify({
+                    query: queryStr,
+                    data: variables
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            if (data.errors) {
+                throw new Error(data.errors[0].message);
+            }
+            return data.data;
+        } catch(error) {
+            console.error('GraphQL query error: ', error);
+            throw error;
+        }
+    }
+
+    async getUserInfo() {
+        const query = `
+            {
+                user {
+                    id
+                    login
+                    email
+                    attrs
+                    auditRatio
+                    
+                }
+                transaction(where: { eventId: { _eq: 75 } }, order_by: {createdAt: desc}) {
+                    amount
+                    createdAt
+                    path
+                    type
+                }
+                progress {
+                    grade
+                }
+                
+            }
+        `;
+        return this.query(query);
+    }
+
+    async getUserXP() {
+        const query = `
+            {
+                transactions( where: { _and: [{ type: { _eq: "xp" } }, { eventId: { _eq: 75 } }]}) {
+                    path amount type createdAt
+                }
+            }
+        `;
+        return this.query(query);
+    }
+
+    async getCompletedExercises(userId, eventId = 75) {
+        const query = `
+        query GetCompleted($userId: Int!, $eventId: Int!) {
+            pendingProgress: progress(
+                where: { isDone: { _eq: true }, eventId: { _eq: $eventId }, userId: { _eq: $userId }, id: { _neq: 145124 } }
+            ) {
+                createdAt path
+            }
+        }`;
+        return this.query(query, { userId, eventId });
+    }
+
+    async getUserSkills() {
+        const query = `
+        {
+            user {
+                id
+                skills: transactions(
+                    where: { type: { _like: "skills_%" } }
+                    order_by: { amount: desc }
+                ) {
+                    type amount
+                }
+            }
+        }
+        `;
+        return this.query(query);
+    }    
+}
